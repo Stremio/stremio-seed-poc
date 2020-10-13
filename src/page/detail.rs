631e641f622d89481d34d2fc7d16e0f2c895fe47
Martin Kavík <martin@kavik.cz>
2020-10-13 13:19:53 +0200

    StreamsList in Rust

--- src/page/detail.rs
+++ src/page/detail.rs
@@ -533,11 +533,30 @@ fn view_streams_list_container<Ms: 'static>(list_style: &Style) -> Node<Ms> {
     div![
         C!["streams-list", "streams-list-container",],
         list_style,
+        s()
+            .display(CssDisplay::Flex)
+            .flex_direction(CssFlexDirection::Column)
+            .background_color(Color::BackgroundDark80),
         div![
             C!["streams-scroll-container",],
+            s()
+                .flex_grow("0")
+                .flex_shrink("1")
+                .flex_basis(CssFlexBasis::Auto)
+                .align_self(CssAlignSelf::Stretch)
+                .overflow_y(CssOverflowY::Auto),
             // stream
             div![
                 C!["stream", "stream-container", "button-container",],
+                s()
+                    .width(styles::global::ITEM_SIZE)
+                    .margin("1rem 2rem"),
+                s()
+                    .first_child()
+                    .margin_top(rem(2)),
+                s()
+                    .last_child()
+                    .margin_bottom("0"),
                 attrs! {
                     At::TabIndex => 0,
                     At::Title => "Google Sample Videos",
@@ -610,6 +629,23 @@ fn view_streams_list_container<Ms: 'static>(list_style: &Style) -> Node<Ms> {
 fn view_install_addons_button<Ms: 'static>() -> Node<Ms> {
     a![
         C!["install-addons-container", "button-container",],
+        s()
+            .flex(CssFlex::None)
+            .align_self(CssAlignSelf::Stretch)
+            .display(CssDisplay::Flex)
+            .flex_direction(CssFlexDirection::Row)
+            .align_items(CssAlignItems::Center)
+            .justify_content(CssJustifyContent::Center)
+            .width(styles::global::ITEM_SIZE)
+            .margin("1rem 2rem 2rem 2rem")
+            .padding(rem(1))
+            .background_color(Color::Signal5),
+        s()
+            .hover()
+            .filter("brightness(1.2)"),
+        s()
+            .focus()
+            .filter("brightness(1.2)"),
         attrs! {
             At::TabIndex => 0,
             At::Title => "Install addons",
@@ -617,6 +653,12 @@ fn view_install_addons_button<Ms: 'static>() -> Node<Ms> {
         },
         svg![
             C!["icon",],
+            s()
+                .flex(CssFlex::None)
+                .width(rem(3))
+                .height(rem(3))
+                .margin_right(rem(1))
+                .fill(Color::SurfaceLighter),
             attrs! {
                 At::ViewBox => "0 0 1043 1024",
                 "icon" => "ic_addons",
@@ -625,7 +667,17 @@ fn view_install_addons_button<Ms: 'static>() -> Node<Ms> {
                 At::D => "M145.468 679.454c-40.056-39.454-80.715-78.908-120.471-118.664-33.431-33.129-33.129-60.235 0-90.353l132.216-129.807c5.693-5.938 12.009-11.201 18.865-15.709l0.411-0.253c23.492-15.059 41.864-7.529 48.188 18.974 0 7.228 2.711 14.758 3.614 22.287 3.801 47.788 37.399 86.785 82.050 98.612l0.773 0.174c10.296 3.123 22.128 4.92 34.381 4.92 36.485 0 69.247-15.94 91.702-41.236l0.11-0.126c24.858-21.654 40.48-53.361 40.48-88.718 0-13.746-2.361-26.941-6.701-39.201l0.254 0.822c-14.354-43.689-53.204-75.339-99.907-78.885l-0.385-0.023c-18.372-2.409-41.562 0-48.188-23.492s11.445-34.635 24.998-47.887q65.054-62.946 130.409-126.795c32.527-31.925 60.235-32.226 90.353 0 40.659 39.153 80.715 78.908 120.471 118.362 8.348 8.594 17.297 16.493 26.82 23.671l0.587 0.424c8.609 7.946 20.158 12.819 32.846 12.819 24.823 0 45.29-18.653 48.148-42.707l0.022-0.229c3.012-13.252 4.518-26.805 8.734-39.755 12.103-42.212 50.358-72.582 95.705-72.582 3.844 0 7.637 0.218 11.368 0.643l-0.456-0.042c54.982 6.832 98.119 49.867 105.048 104.211l0.062 0.598c0.139 1.948 0.218 4.221 0.218 6.512 0 45.084-30.574 83.026-72.118 94.226l-0.683 0.157c-12.348 3.915-25.299 5.722-37.948 8.433-45.779 9.638-60.235 46.984-30.118 82.824 15.265 17.569 30.806 33.587 47.177 48.718l0.409 0.373c31.925 31.925 64.452 62.946 96.075 94.871 13.698 9.715 22.53 25.511 22.53 43.369s-8.832 33.655-22.366 43.259l-0.164 0.111c-45.176 45.176-90.353 90.353-137.035 134.325-5.672 5.996-12.106 11.184-19.169 15.434l-0.408 0.227c-4.663 3.903-10.725 6.273-17.341 6.273-13.891 0-25.341-10.449-26.92-23.915l-0.012-0.127c-2.019-7.447-3.714-16.45-4.742-25.655l-0.077-0.848c-4.119-47.717-38.088-86.476-82.967-97.721l-0.76-0.161c-9.584-2.63-20.589-4.141-31.947-4.141-39.149 0-74.105 17.956-97.080 46.081l-0.178 0.225c-21.801 21.801-35.285 51.918-35.285 85.185 0 1.182 0.017 2.36 0.051 3.533l-0.004-0.172c1.534 53.671 40.587 97.786 91.776 107.115l0.685 0.104c12.649 2.409 25.901 3.313 38.249 6.626 22.588 6.325 30.118 21.685 18.372 41.864-4.976 8.015-10.653 14.937-17.116 21.035l-0.051 0.047c-44.875 44.574-90.353 90.353-135.228 133.12-10.241 14.067-26.653 23.106-45.176 23.106s-34.935-9.039-45.066-22.946l-0.111-0.159c-40.659-38.852-80.414-78.908-120.471-118.362z"
             }],
         ],
-        div![C!["label",], "Install addons",]
+        div![
+            C!["label",], 
+            s()
+                .flex_grow("0")
+                .flex_shrink("1")
+                .flex_basis(CssFlexBasis::Auto)
+                .font_size(rem(1.5))
+                .max_height(em(3.6))
+                .color(Color::SurfaceLighter),
+            "Install addons",
+        ]
     ]
 }
 
