f576e5fa35f0cac4c7e39b2bea04757296d6807c
Martin Kavík <martin@kavik.cz>
2020-11-27 17:11:47 +0100

    discover colors

--- src/styles/themes.rs
+++ src/styles/themes.rs
@@ -187,6 +187,7 @@ pub enum Color {
     SecondaryLight5_90,
     SecondaryVariant1_90,
     SecondaryVariant1Light1_90,
+    SecondaryVariant2Light1,
     SecondaryVariant2Light1_90,
     SecondaryVariant2Light2_90,
     SurfaceLight5,
@@ -331,6 +332,7 @@ pub fn default_color_theme() -> Theme {
         .set_color(SecondaryLight5_90, hsla(226.6, 37.2, 61.9, 0.9))
         .set_color(SecondaryVariant1_90, hsla(224.3, 42.1, 66, 0.9))
         .set_color(SecondaryVariant1Light1_90, hsla(224.3, 42.1, 71, 0.9))
+        .set_color(SecondaryVariant2Light1, hsl(222.8, 100, 78))
         .set_color(SecondaryVariant2Light1_90, hsla(222.8, 100, 78, 0.9))
         .set_color(SecondaryVariant2Light2_90, hsla(222.8, 100, 83, 0.9))
         .set_color(SurfaceLight5, hsl(0, 0, 100))
