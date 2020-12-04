c90cd13aed38ba80503a58e341600926339b5743
Martin Kavík <martin@kavik.cz>
2020-12-04 21:03:25 +0100

    type_selector

--- src/page/discover/type_selector.rs
+++ src/page/discover/type_selector.rs
@@ -1,53 +1,38 @@
 use crate::entity::multi_select;
 use seed::{*, prelude::*};
 use std::fmt::Debug;
-use stremio_core::state_types::TypeEntry;
+use std::rc::Rc;
+use stremio_core::state_types::{CatalogFiltered, TypeEntry};
+use stremio_core::types::MetaPreview;
 use stremio_core::types::addons::ResourceRequest;
 
 // ------ ------
 //     View
 // ------ ------
 
-// pub fn view<T: Clone>(model: &Model, groups: &[multi_select::Group<T>]) -> Node<Msg> {
-//     // multi_select::view(&model.0, groups).map_msg(Msg)
-//     empty![]
-// }
+pub fn view<Ms: 'static>(
+    catalog: &CatalogFiltered<MetaPreview>,
+    send_res_req_msg: impl Fn(ResourceRequest) -> Ms + 'static + Copy,
+) -> Node<Ms> {
+    let items = items(catalog, send_res_req_msg);
+    multi_select::view("Select type", items)
+}
 
-// ------ ------
-//  Conversion
-// ------ ------
-
-// pub fn groups(type_entries: &[TypeEntry]) -> Vec<multi_select::Group<TypeEntry>> {
-//     let items = type_entries
-//         .iter()
-//         .map(|type_entry| multi_select::GroupItem {
-//             id: type_entry.type_name.clone(),
-//             label: type_entry.type_name.clone(),
-//             selected: type_entry.is_selected,
-//             value: type_entry.clone(),
-//         })
-//         .collect::<Vec<_>>();
-
-//     vec![multi_select::Group {
-//         id: "default".to_owned(),
-//         label: None,
-//         items,
-//         limit: 1,
-//         required: true,
-//     }]
-// }
-
-// pub fn resource_request(
-//     groups_with_selected_items: Vec<multi_select::Group<TypeEntry>>,
-// ) -> ResourceRequest {
-//     groups_with_selected_items
-//         .into_iter()
-//         .next()
-//         .expect("type selector's group `default`")
-//         .items
-//         .into_iter()
-//         .next()
-//         .expect("type selector's selected item")
-//         .value
-//         .load
-// }
+pub fn items<Ms: 'static>(
+    catalog: &CatalogFiltered<MetaPreview>,
+    send_res_req_msg: impl Fn(ResourceRequest) -> Ms + 'static + Copy,
+) -> Vec<multi_select::Item<Ms>> {
+    catalog.types
+        .iter()
+        .map(|type_entry| {
+            let res_req = type_entry.load.clone();
+            multi_select::Item {
+                title: type_entry.type_name.clone(),
+                selected: type_entry.is_selected,
+                on_click: Rc::new(move || {
+                    send_res_req_msg(res_req.clone())
+                }),
+            }
+        })
+        .collect()
+}
