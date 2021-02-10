4f45640371a5ab4a8303b9f80f583ed1053b2530
Martin Kavík <martin@kavik.cz>
2021-02-10 17:42:56 +0100

    Library skeleton

--- src/page/library.rs
+++ src/page/library.rs
@@ -1,11 +1,89 @@
 use seed::{prelude::*, *};
+use seed_hooks::{*, topo::nested as view};
+use seed_styles::{em, pc, rem, Style};
+use seed_styles::*;
+use crate::{multi_select, Msg as RootMsg, Context, PageId, Actions, Urls as RootUrls};
+use crate::basic_layout::{basic_layout, BasicLayoutArgs};
+use crate::styles::{self, themes::{Color, Breakpoint}, global};
+
+// ------ ------
+//     Init
+// ------ ------
+
+pub fn init(
+    mut url: Url,
+    model: &mut Option<Model>,
+    context: &mut Context,
+    orders: &mut impl Orders<Msg>,
+) -> Option<PageId> {
+    let base_url = url.to_hash_base_url();
+
+    let model = model.get_or_insert_with(move || Model {
+    });
+    Some(PageId::Library)
+}
+
+// ------ ------
+//     Model
+// ------ ------
+
+pub struct Model {
+}
+
+// ------ ------
+//     Urls
+// ------ ------
+
+struct_urls!();
+impl<'a> Urls<'a> {
+    pub fn root(self) -> Url {
+        self.base_url()
+    }
+}
+
+// ------ ------
+//    Update
+// ------ ------
+
+pub enum Msg {
+}
+
+pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+    }
+}
 
 // ------ ------
 //     View
 // ------ ------
 
-pub fn view<Ms: 'static>() -> Node<Ms> {
+#[view]
+pub fn view(model: &Model, context: &Context, page_id: PageId, msg_mapper: fn(Msg) -> RootMsg) -> Node<RootMsg> {
+    basic_layout(BasicLayoutArgs {
+        page_content: library_content(model, context).map_msg(msg_mapper),
+        container_class: "library-container",
+        context,
+        page_id,
+        search_args: None,
+    })
+}
+
+#[view]
+fn library_content<'a>(model: &Model, context: &Context) -> Node<Msg> {
     div![
-        "Library (WIP)"
+        C!["library-content"],
+        s()
+            .display(CssDisplay::Flex)
+            .flex_direction(CssFlexDirection::Row)
+            .height(pc(100))
+            .width(pc(100)),            
+        message_container(),
+    ]
+}
+
+#[view]
+fn message_container() -> Node<Msg> {
+    div![
+        "library content"
     ]
 }
