a21218b5443346c36d84c73734a05052d5930bfb
Martin Kavík <martin@kavik.cz>
2021-05-26 17:33:05 +0200

    youtube video custom element

--- src/page/player.rs
+++ src/page/player.rs
@@ -1,6 +1,13 @@
 use seed::{prelude::*, *};
-use crate::{PageId, Context};
-use stremio_core::types::resource::Stream;
+use seed_hooks::{*, topo::nested as view};
+use seed_styles::{em, pc, rem, Style};
+use seed_styles::*;
+use std::rc::Rc;
+use std::array;
+use crate::{PageId, Context, Actions};
+use stremio_core::types::resource::{Stream, StreamSource};
+use stremio_core::models::player::Selected as PlayerSelected;
+use stremio_core::runtime::msg::{Action, ActionLoad, Msg as CoreMsg};
 
 // ------ ------
 //     Init
@@ -15,12 +22,25 @@ pub fn init(
     let base_url = url.to_hash_base_url();
 
     let stream: Stream = serde_json::from_str(url.next_hash_path_part()?).ok()?;
-    log!(stream);
+
+    load_player(stream, orders);
 
     let mut model = model.get_or_insert_with(move || Model {
         base_url,
     });
     Some(PageId::Player)
+}
+
+fn load_player(stream: Stream, orders: &mut impl Orders<Msg>) {
+    let player_selected = PlayerSelected {
+        stream,
+        meta_request: None,
+        stream_request: None,
+        subtitles_path: None
+    };
+    orders.notify(Actions::UpdateCoreModel(Rc::new(CoreMsg::Action(Action::Load(
+        ActionLoad::Player(player_selected),
+    )))));
 }
 
 // ------ ------
@@ -60,18 +80,53 @@ pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 //     View
 // ------ ------
 
-pub fn view(model: &Model) -> Node<Msg> {
-    div![video![
-        style! {
-            St::MaxWidth => unit!(100, %),
-            St::Height => "auto",
-        },
-        attrs! {
-            At::Controls => AtValue::None,
-        },
-        source![attrs! {
-            At::Src => "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-            At::Type => "video/mp4",
-        }]
-    ]]
+#[view]
+pub fn view(model: &Model, context: &Context) -> Node<Msg> {
+    match context.core_model.player.selected.as_ref().map(|selected| &selected.stream.source) {
+        Some(stream_source) => div![
+            s()
+                .background("black")
+                .width(pc(100))
+                .height(pc(100))
+                .color("white"),
+            video_container(stream_source),
+        ],
+        None => div!["Loading..."],
+    }
 }
+
+#[view]
+fn video_container(stream_source: &StreamSource) -> Node<Msg> {
+    match stream_source {
+        StreamSource::YouTube { yt_id } => youtube_video(yt_id),
+        _ => div!["Stream not supported"],
+    }
+}
+
+#[view]
+fn youtube_video(yt_id: &str) -> Node<Msg> {
+    let params = UrlSearch::new(array::IntoIter::new([
+        ("autoplay", 1),
+        ("cc_load_policy", 3),
+        ("controls", 0),
+        ("disablekb", 1),
+        ("enablejsapi", 1),
+        ("fs", 0),
+        ("iv_load_policy", 3),
+        ("loop", 0),
+        ("modestbranding", 1),
+        ("rel", 0),
+    ]).map(|(key, value)| (key, array::IntoIter::new([value.to_string()]))));
+
+    div![
+        // https://github.com/paulirish/lite-youtube-embed
+        custom![
+            Tag::from("lite-youtube"),
+            attrs!{
+                At::from("videoid") => yt_id,
+                At::from("params") => params
+            }
+        ]
+    ]
+}
+
