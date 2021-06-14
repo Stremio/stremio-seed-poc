8949b8c405d6f5b93f73d23947b55f084354466e
Martin Kavík <martin@kavik.cz>
2021-06-14 23:17:53 +0200

    serde-wasm-bindgen, Player::new

--- src/page/player.rs
+++ src/page/player.rs
@@ -6,6 +6,7 @@ use web_sys::HtmlElement;
 use std::rc::Rc;
 use std::array;
 use enclose::enc;
+use serde::Serialize;
 use crate::{PageId, Context, Actions};
 use stremio_core::types::resource::{Stream, StreamSource};
 use stremio_core::models::player::Selected as PlayerSelected;
@@ -25,22 +26,18 @@ pub fn init(
 
     let stream: Stream = serde_json::from_str(url.next_hash_path_part()?).ok()?;
 
-    load_player(stream, orders);
+    load_player(stream.clone(), orders);
 
+    orders.after_next_render(|_| Msg::Rendered);
+    
     let mut model = model.get_or_insert_with(move || Model {
         base_url,
         container_ref: ElRef::new(),
-        core_msg_sub_handle: None,
         yt_on_api_loaded: None,
         yt_on_api_error: None,
         yt_on_ready: None,
+        stream,
     });
-
-    if context.core_model.player.selected.is_some() {
-        orders.send_msg(Msg::PlayerSelected);
-    } else {
-        model.core_msg_sub_handle = Some(orders.subscribe_with_handle(Msg::CoreMsg));
-    }
     Some(PageId::Player)
 }
 
@@ -63,10 +60,10 @@ fn load_player(stream: Stream, orders: &mut impl Orders<Msg>) {
 pub struct Model {
     base_url: Url,
     container_ref: ElRef<HtmlElement>,
-    core_msg_sub_handle: Option<SubHandle>,
     yt_on_api_loaded: Option<Closure<dyn Fn()>>,
     yt_on_api_error: Option<Closure<dyn Fn()>>,
     yt_on_ready: Option<Rc<Closure<dyn Fn()>>>,
+    stream: Stream,
 }
 
 // ------ ------
@@ -85,37 +82,47 @@ impl<'a> Urls<'a> {
 // ------ ------
 
 pub enum Msg {
-    CoreMsg(Rc<CoreMsg>),
-    PlayerSelected,
-    YoutubeReady(Rc<HtmlElement>),
+    Rendered,
+    YoutubeReady(Rc<HtmlElement>, String),
 }
 
 pub fn update(msg: Msg, model: &mut Model, context: &mut Context, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::CoreMsg(core_msg) => {
-            if let CoreMsg::Internal(_) = core_msg.as_ref() {
-                if context.core_model.player.selected.is_some() {
-                    orders.after_next_render(|_| Msg::PlayerSelected);
-                    model.core_msg_sub_handle = None;
-                }
-            }
-        }
-        Msg::PlayerSelected => {
-            let stream_source = &context.core_model.player.selected.as_ref().unwrap().stream.source;
-            match stream_source {
+        Msg::Rendered => {
+            match model.stream.source.clone() {
                 StreamSource::YouTube { yt_id } => {
                     init_youtube(model, yt_id, orders)
                 }
                 stream_source => error!("Unhandled stream source:", stream_source),
             }
         }
-        Msg::YoutubeReady(video_container) => {
-            log!("YoutubeReady!!!")
+        Msg::YoutubeReady(video_container, yt_id) => {
+            let config = PlayerConfig {
+                width: "100%",
+                height: "100%",
+                video_id: &yt_id,
+                player_vars: PlayerVars {
+                    autoplay: 1,
+                    cc_load_policy: 3,
+                    controls: 0,
+                    disablekb: 1,
+                    enablejsapi: 1,
+                    fs: 0,
+                    iv_load_policy: 3,
+                    r#loop: 0,
+                    modestbranding: 1,
+                    playsinline: 1,
+                    rel: 0
+                },
+            };
+            let config = serde_wasm_bindgen::to_value(&config).unwrap();
+            log!("Youtube config:", config);
+            let video = Player::new(&video_container, &config);
         }
     }
 }
 
-fn init_youtube(model: &mut Model, yt_id: &str, orders: &mut impl Orders<Msg>) {
+fn init_youtube(model: &mut Model, yt_id: String, orders: &mut impl Orders<Msg>) {
     let container = model.container_ref.get().expect("video container");
 
     let video_container = document().create_element("div").unwrap().unchecked_into::<web_sys::HtmlElement>();
@@ -131,7 +138,7 @@ fn init_youtube(model: &mut Model, yt_id: &str, orders: &mut impl Orders<Msg>) {
 
     let sender = orders.msg_sender();
     let on_ready = enc!((video_container) move || {
-        sender(Some(Msg::YoutubeReady(video_container.clone())));
+        sender(Some(Msg::YoutubeReady(video_container.clone(), yt_id.clone())));
     });
     let on_ready = Rc::new(Closure::wrap(Box::new(on_ready) as Box<dyn Fn()>));
 
@@ -157,37 +164,40 @@ fn init_youtube(model: &mut Model, yt_id: &str, orders: &mut impl Orders<Msg>) {
 #[wasm_bindgen]
 extern "C" {
     type YT;
-
     #[wasm_bindgen(static_method_of = YT)]
     pub fn ready(ready: &js_sys::Function);
 }
 
-// #[view]
-// fn youtube_video(yt_id: &str) -> Node<Msg> {
-//     let params = UrlSearch::new(array::IntoIter::new([
-//         ("autoplay", 1),
-//         ("cc_load_policy", 3),
-//         ("controls", 0),
-//         ("disablekb", 1),
-//         ("enablejsapi", 1),
-//         ("fs", 0),
-//         ("iv_load_policy", 3),
-//         ("loop", 0),
-//         ("modestbranding", 1),
-//         ("rel", 0),
-//     ]).map(|(key, value)| (key, array::IntoIter::new([value.to_string()]))));
+#[wasm_bindgen]
+extern "C" {
+    type Player;
+    #[wasm_bindgen(constructor, js_namespace = YT)]
+    pub fn new(video_container: &web_sys::HtmlElement, config: &JsValue) -> Player;
+}
 
-//     div![
-//         // https://github.com/paulirish/lite-youtube-embed
-//         custom![
-//             Tag::from("lite-youtube"),
-//             attrs!{
-//                 At::from("videoid") => yt_id,
-//                 At::from("params") => params
-//             }
-//         ]
-//     ]
-// }
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct PlayerConfig<'a> {
+    width: &'static str,
+    height: &'static str,
+    video_id: &'a str,
+    player_vars: PlayerVars,
+}
+
+#[derive(Serialize)]
+struct PlayerVars {
+    autoplay: u8,
+    cc_load_policy: u8,
+    controls: u8,
+    disablekb: u8,
+    enablejsapi: u8,
+    fs: u8,
+    iv_load_policy: u8,
+    r#loop: u8,
+    modestbranding: u8,
+    playsinline: u8,
+    rel: u8,
+}
 
 // ------ ------
 //     View
@@ -197,14 +207,12 @@ extern "C" {
 pub fn view(model: &Model, context: &Context) -> Node<Msg> {
     if context.core_model.player.selected.is_some() {
         div![
+            el_ref(&model.container_ref),
             s()
                 .background("black")
                 .width(pc(100))
                 .height(pc(100))
                 .color("white"),
-            div![
-                el_ref(&model.container_ref),
-            ],
         ]
     } else {
         div!["Loading..."]
